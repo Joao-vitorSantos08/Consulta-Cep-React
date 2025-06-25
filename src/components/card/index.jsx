80c200734346803716faa9c69087ec917553991f
João <joaovitorsantossouza937@gmail.com>
2025-06-24 21:42:19 -0300

    feat: creates the basic structure of the project

---
+++ src/components/card/index.jsx
@@ -0,0 +1,24 @@
+import './style.css'
+
+const Card = () =>{
+    return(
+    <div class="container">
+    <h1>🔍 Consulta de CEP</h1>
+
+    <form id="cep-form">
+      <input  type="text" id="input_cep" placeholder="Digite o CEP" maxlength="9" required/>
+      <button type="submit" id="btn-cep">Buscar</button>
+    </form>
+
+    <div class="resultado" id="resultado">
+      <p><strong>Logradouro:</strong> <span id="logradouro">--</span></p>
+      <p><strong>Bairro:</strong> <span id="bairro">--</span></p>
+      <p><strong>Região:</strong> <span id="regiao">--</span></p>
+      <p><strong>Estado:</strong> <span id="estado">--</span></p>
+    </div>
+  </div>
+  
+    )
+}
+
+export default Card
